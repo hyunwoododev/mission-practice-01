f51e2a7aada5b3dd76527cd39d67d48d294bf91b
hyunwoo-do <121751640+hyunwoododev@users.noreply.github.com>
2024-01-29 20:56:55 +0900

    feat: router setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from '@app/database';
+import { ComicsModule } from './comics/comics.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, ComicsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
